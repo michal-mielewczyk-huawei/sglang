7940a43478876a16d8994fdd2963b46ed5243d24
Michal Mielewczyk <michal.mielewczyk@huawei.com>
2026-07-13 07:40:05 -0700

    restore big datasets

--- test/registered/ascend/performance/qwen3_30b_a3b/test_npu_qwen3_30b_bf16_1p_in1k_out100.py
+++ test/registered/ascend/performance/qwen3_30b_a3b/test_npu_qwen3_30b_bf16_1p_in1k_out100.py
@@ -123,12 +123,11 @@ class TestKVTCQwen30B_dump_openmath(TestAscendPerformanceTestCaseBase):
     model = QWEN3_30B_A3B_MODEL_PATH
     other_args = OTHER_ARGS
     envs = ENVS
-    openmath_parts = 1
-    kvtc_remote_address = ["https://huggingface.co/datasets/nvidia/OpenMathReasoning/resolve/main/data/additional_problems-00000-of-00001.parquet"]
-    #remote_address = [
-    #        f"https://huggingface.co/datasets/nvidia/OpenMathReasoning/resolve/main/data/cot-00{i:03d}-of-00144.parquet"
-    #        for i in range(openmath_parts)
-    #        ]
+    openmath_parts = 144
+    remote_address = [
+            f"https://huggingface.co/datasets/nvidia/OpenMathReasoning/resolve/main/data/cot-00{i:03d}-of-00144.parquet"
+            for i in range(openmath_parts)
+            ]
     kvtc_dataset_name = "openmath"
     client_concurrency = 16
 
@@ -179,8 +178,7 @@ class TestKVTCQwen30B_dump_fineweb(TestAscendPerformanceTestCaseBase):
     model = QWEN3_0_6B_MODEL_PATH
     other_args = OTHER_ARGS
     envs = ENVS
-    kvtc_remote_address = ["https://huggingface.co/datasets/nvidia/OpenMathReasoning/resolve/main/data/additional_problems-00000-of-00001.parquet"]
-    #kvtc_remote_address = ["https://huggingface.co/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2025-26/000_00000.parquet"]
+    kvtc_remote_address = ["https://huggingface.co/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2025-26/000_00000.parquet"]
     kvtc_dataset_name = "fineweb"
     client_concurrency = 16
 
